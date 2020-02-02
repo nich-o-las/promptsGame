bd8208253b9ec4e36015a2bc1ae03a006dd6242d
Nicholas Cox <nick.a.cox@gmail.com>
2020-02-02 17:43:17 -0600

    heroku tweaks

--- server.js
+++ server.js
@@ -29,6 +29,16 @@ app.use(function(err, req, res, next) {
     }
 });
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 const Game = require('./models/game');
 
 Game.deleteMany({}, (err, res) => {});
